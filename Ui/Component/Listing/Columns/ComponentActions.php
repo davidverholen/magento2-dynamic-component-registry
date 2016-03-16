25909f332ac488df8b0fabd10c9bf0e54e102e21
David Verholen <david@verholen.com>
2016-03-16 12:43:20 +0100

    fix dataprovider, use select fields for type and status in listing

---
+++ Ui/Component/Listing/Columns/ComponentActions.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace DavidVerholen\DynamicComponentRegistry\Ui\Component\Listing\Columns;
+
+use Magento\Framework\View\Element\UiComponent\ContextInterface;
+use Magento\Framework\View\Element\UiComponentFactory;
+use Magento\Ui\Component\Listing\Columns\Column;
+use Magento\Framework\UrlInterface;
+
+class ComponentActions extends Column
+{
+    /**
+     * @var UrlInterface
+     */
+    protected $urlBuilder;
+
+    /**
+     * @param ContextInterface $context
+     * @param UiComponentFactory $uiComponentFactory
+     * @param UrlInterface $urlBuilder
+     * @param array $components
+     * @param array $data
+     */
+    public function __construct(
+        ContextInterface $context,
+        UiComponentFactory $uiComponentFactory,
+        UrlInterface $urlBuilder,
+        array $components = [],
+        array $data = []
+    ) {
+        parent::__construct($context, $uiComponentFactory, $components, $data);
+        $this->urlBuilder = $urlBuilder;
+    }
+
+    /**
+     * Prepare Data Source
+     *
+     * @param array $dataSource
+     * @return array
+     */
+    public function prepareDataSource(array $dataSource)
+    {
+        if (isset($dataSource['data']['items'])) {
+            foreach ($dataSource['data']['items'] as &$item) {
+                $item[$this->getData('name')]['edit'] = [
+                    'href' => $this->urlBuilder->getUrl(
+                        'dynamic_component_registry/component/edit',
+                        ['id' => $item['component_id']]
+                    ),
+                    'label' => __('Edit'),
+                    'hidden' => false,
+                ];
+            }
+        }
+        return $dataSource;
+    }
+}
