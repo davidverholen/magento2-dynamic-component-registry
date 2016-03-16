98bc56fab4855b997d46ede7daba2ea9d1dbad1b
David Verholen <david@verholen.com>
2016-03-16 12:58:18 +0100

    add delete url to delete button

--- Block/Adminhtml/Component/Edit/DeleteButton.php
+++ Block/Adminhtml/Component/Edit/DeleteButton.php
@@ -2,10 +2,35 @@
 
 namespace DavidVerholen\DynamicComponentRegistry\Block\Adminhtml\Component\Edit;
 
+use DavidVerholen\DynamicComponentRegistry\Model\Component\Builder;
+use Magento\Framework\Registry;
+use Magento\Framework\UrlInterface;
 use Magento\Framework\View\Element\UiComponent\Control\ButtonProviderInterface;
 
 class DeleteButton implements ButtonProviderInterface
 {
+    /**
+     * @var Registry
+     */
+    protected $registry;
+
+    /**
+     * @var UrlInterface
+     */
+    protected $urlBuilder;
+
+    /**
+     * DeleteButton constructor.
+     *
+     * @param Registry     $registry
+     * @param UrlInterface $urlBuilder
+     */
+    public function __construct(Registry $registry, UrlInterface $urlBuilder)
+    {
+        $this->registry = $registry;
+        $this->urlBuilder = $urlBuilder;
+    }
+
     /**
      * @return array
      */
@@ -15,6 +40,9 @@ class DeleteButton implements ButtonProviderInterface
             'id' => 'delete',
             'label' => __('Delete'),
             'class' => 'delete',
+            'url' => $this->urlBuilder->getUrl('*/*/delete', ['_query' => [
+                'id' => $this->registry->registry(Builder::CURRENT_COMPONENT_ID)
+            ]]),
             'sort_order' => 10
         ];
     }
