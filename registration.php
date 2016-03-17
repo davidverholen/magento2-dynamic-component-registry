d5b0c2eb74f55d9243f197059796c6fab0838ed6
David Verholen <david@verholen.com>
2016-03-17 14:26:36 +0100

    add basic error handling for broken configuration file

--- registration.php
+++ registration.php
@@ -5,6 +5,7 @@ use DavidVerholen\DynamicComponentRegistry\Serializable\DynamicComponentsConfig;
 use Doctrine\Common\Annotations\AnnotationRegistry;
 use JMS\Serializer\SerializerBuilder;
 use Magento\Framework\App\Filesystem\DirectoryList;
+use Magento\Framework\App\ObjectManager;
 use Magento\Framework\Component\ComponentRegistrar;
 
 ComponentRegistrar::register(
@@ -23,12 +24,24 @@ if (defined('BP')) {
 
     if (file_exists($configFilePath)) {
         AnnotationRegistry::registerLoader('class_exists');
-        /** @var DynamicComponentsConfig $dynamicComponentsConfig */
-        $dynamicComponentsConfig = SerializerBuilder::create()->build()->deserialize(
-            file_get_contents($configFilePath),
-            DynamicComponentsConfig::class,
-            ConfigFactory::CONFIG_FORMAT
-        );
+
+        try {
+            /** @var DynamicComponentsConfig $dynamicComponentsConfig */
+            $dynamicComponentsConfig = SerializerBuilder::create()
+                ->build()
+                ->deserialize(
+                    file_get_contents($configFilePath),
+                    DynamicComponentsConfig::class,
+                    ConfigFactory::CONFIG_FORMAT
+                );
+        } catch (Exception $e) {
+            printf('<h3>Dynamic Component Registry - Broken Configuration File</h3>');
+            printf('<p>error while reading Dynamic Components Config: <b>%s</b></p>', $e->getMessage());
+            printf('<p>1. Delete <b>\'%s\'</b> to get the Page running again<br/>', $configFilePath);
+            printf('2. You also may have to deactivate dynamically loaded Modules by removing the entry from <b>\'%s\'</b><br/>', implode(DIRECTORY_SEPARATOR, [BP, 'app', 'etc', 'config.php']));
+            printf('3. Afterwards save any Dynamic Component in Backend to recreate the Configuration File</p>');
+            exit;
+        }
 
         /** @var \Composer\Autoload\ClassLoader $composerAutoloader */
         $composerAutoloader = null;
